64a7dae840f0af4d64c5aa20254301a09fd89b1d
joyadonia <joyadonia@gmail.com>
2025-05-07 16:45:32 +0200

    wip

--- Keycloak.Poc/Program.cs
+++ Keycloak.Poc/Program.cs
@@ -44,7 +44,7 @@ builder.Services.AddAuthentication(options =>
                 // Local development settings
                 //options.Authority = "http://keycloak:8080/realms/YAMS";
                 //options.Authority = "http://host.docker.internal:8081/realms/YAMS";
-                options.Authority = "http://ngrp/realms/YAMS";
+                options.Authority = "http://ngrp/keycloak/realms/YAMS";
                  //options.Authority = "http://localhost:9010/realms/YAMS";
                 //options.Authority = "http://ngrp/realms/YAMS";
 
@@ -71,14 +71,14 @@ builder.Services.AddAuthentication(options =>
             options.Events = new OpenIdConnectEvents
             {
                 OnTokenValidated = MapRoles,
-                OnRedirectToIdentityProvider = context =>
-                {
-                    // Override the authority for redirect URIs going to the browser
-                    context.ProtocolMessage.IssuerAddress = context.ProtocolMessage.IssuerAddress
-                        .Replace("http://ngrp", "http://localhost:8080");
+                // OnRedirectToIdentityProvider = context =>
+                // {
+                //     // Override the authority for redirect URIs going to the browser
+                //     context.ProtocolMessage.IssuerAddress = context.ProtocolMessage.IssuerAddress
+                //         .Replace("http://ngrp", "http://localhost:8080");
 
-                    return Task.CompletedTask;
-                }
+                //     return Task.CompletedTask;
+                // }
             };
         }
         catch (Exception ex)
